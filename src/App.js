316d24fa6b96ada44c5dc1811c92870d85646fec
eyad-op <43111541+eyad-op@users.noreply.github.com>
2022-10-13 11:31:10 +0300

    show score

--- src/App.js
+++ src/App.js
@@ -7,41 +7,47 @@ function App() {
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
+  const [showScore, setShowScore] = useState(false);
+
   const handleAnswerButtonClick = () => {
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
-      alert("You have answered all of the questions");
+      setShowScore(true);
     }
   };
 
   return (
     <div className="container">
-      <div className="question">
-        <div className="question__body">
-          <h2 className="question__number">
-            Question {currentQuestion + 1}
-            <span>/{questions.length}</span>
-          </h2>
-          <h1 className="question__text">
-            {questions[currentQuestion].questionText}
-          </h1>
+      {showScore ? (
+        <h1 className="score">You have scored 1 out of {questions.length}</h1>
+      ) : (
+        <div className="question">
+          <div className="question__body">
+            <h2 className="question__number">
+              Question {currentQuestion + 1}
+              <span>/{questions.length}</span>
+            </h2>
+            <h1 className="question__text">
+              {questions[currentQuestion].questionText}
+            </h1>
+          </div>
+          <div className="question__answers">
+            {questions[currentQuestion].answerOptions.map((answer, index) => {
+              return (
+                <button
+                  onClick={handleAnswerButtonClick}
+                  key={index}
+                  className="question__answer"
+                >
+                  {answer.answerText}
+                </button>
+              );
+            })}
+          </div>
         </div>
-        <div className="question__answers">
-          {questions[currentQuestion].answerOptions.map((answer, index) => {
-            return (
-              <button
-                onClick={handleAnswerButtonClick}
-                key={index}
-                className="question__answer"
-              >
-                {answer.answerText}
-              </button>
-            );
-          })}
-        </div>
-      </div>
+      )}
     </div>
   );
 }
