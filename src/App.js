61c6039a01e49620e4f032ad2dbb450638a62e69
eyad-op <43111541+eyad-op@users.noreply.github.com>
2022-10-13 11:14:15 +0300

    showing questions and answers dynamically

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
+import { useState } from "react";
 import "./App.scss";
+import Questions from "./data.json";
 
 function App() {
+  const questions = Questions.questions;
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleAnswerButtonClick = () => {
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      alert("You have answered all of the questions");
+    }
+  };
+
   return (
     <div className="container">
       <div className="question">
         <div className="question__body">
-          <h1 className="question__number">
-            Question 1<span>/4</span>
+          <h2 className="question__number">
+            Question {currentQuestion + 1}
+            <span>/{questions.length}</span>
+          </h2>
+          <h1 className="question__text">
+            {questions[currentQuestion].questionText}
           </h1>
-          <h2 className="question__text">What is the capital of France</h2>
         </div>
         <div className="question__answers">
-          <button className="question__answer">New York</button>
-          <button className="question__answer">London</button>
-          <button className="question__answer">Paris</button>
-          <button className="question__answer">Dublin</button>
+          {questions[currentQuestion].answerOptions.map((answer, index) => {
+            return (
+              <button
+                onClick={handleAnswerButtonClick}
+                key={index}
+                className="question__answer"
+              >
+                {answer.answerText}
+              </button>
+            );
+          })}
         </div>
       </div>
     </div>
